f534b6410645479e76ad9353c1dd90d98bc06564
losnappas <karki.ossi.o@student.uta.fi>
2017-10-16 12:28:43 +0300

    cleaning

---
+++ Assets/FrisbeeAssets/Scripts/FrisbeeLocation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrisbeeLocation{
+    public Quaternion rot;
+    public Vector3 pos;
+
+    public FrisbeeLocation(Quaternion rot, Vector3 pos)
+    {
+        this.rot = rot;
+        this.pos = pos;
+    }
+}
