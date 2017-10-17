334cc0742d5b1d917b7293b3204ac9c8baf81bcd
John Mäkelä <aleirenth@gmail.com>
2017-10-17 11:45:40 +0300

    Added timestamp, constructors to FrisbeeLocation

--- Assets/FrisbeeAssets/Scripts/FrisbeeLocation.cs
+++ Assets/FrisbeeAssets/Scripts/FrisbeeLocation.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class FrisbeeLocation{
     public Quaternion rot;
     public Vector3 pos;
+    public float time = 0F;
+
+    public FrisbeeLocation()
+    {
+        this.rot = new Quaternion(0F, 0F, 0F, 0F);
+        this.pos = new Vector3(0F, 0F, 0F);
+        this.time = 0F;
+    }
 
     public FrisbeeLocation(Quaternion rot, Vector3 pos)
     {
         this.rot = rot;
         this.pos = pos;
+    }
+    public FrisbeeLocation(Quaternion rot, Vector3 pos, float time)
+    {
+        this.rot = rot;
+        this.pos = pos;
+        this.time = time;
     }
 }
